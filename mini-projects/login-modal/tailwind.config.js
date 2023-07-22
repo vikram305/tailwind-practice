3d3ff59f4145c35d4ff3daabc597892b87027ee6
vikrammaurya <vikram@appgurus.clouddownunder.com.au>
2023-07-22 09:55:15 +0530

    first commit

---
+++ mini-projects/login-modal/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    fontFamily: {
+      sans: ["Mulish", "sans-serif"],
+      mono: ["Rokkitt", "monospace"],
+    },
+    extend: {},
+  },
+  plugins: [],
+};
